de6ac48187e3e2e9a2a9a75e9dba8b83c47168ae
admin <admin@USER>
2016-06-10 14:35:58 -0400

    made class Card and Suit

---
+++ Lab_3/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Card
+    {
+        // ++++++++++++++++++++++++++++++++PUBLIC PROPERTIES ++++++++++++++++++++++++++++
+        //These are the enums that come from the Faces and Suits enum Class.
+        public Faces FaceFromEnum { get; set; }
+        public Suits SuitFromEnum { get; set; }
+
+        /**
+         * <summary>
+         * This constructor takes two paramaters: face and suit to make the card objet
+         * </summary>
+         * 
+         * @constructor Card
+         * @ param {Faces} faceParamFromCtor
+         * @ param {Suits} suitParamFromCtor
+         */
+        public Card(Faces faceParamFromCtor, Suits suitParamFromctor)
+        {
+            this.FaceFromEnum = faceParamFromCtor;
+            this.SuitFromEnum = suitParamFromctor;
+        }
+    }
+}
---
+++ Lab_3/Faces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+   
+  public enum Faces
+    {
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+
+    }
+}
